7f8e6669a5ecdbe3a486253f6c3f41ecde2d7316
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-09-21 21:53:24 +0900

    🎆Style : change default colors

--- src/atom.js
+++ src/atom.js
@@ -2,5 +2,5 @@ import { atom } from "recoil";
 
 export const colorCardAtom = atom({
   key: "colorCard",
-  default: ["#f92ecc", "#4877cc", "#1994bb", "#c261c0", "#67e5c7"],
+  default: ["#f92ecc", "#4877cc", "#67e5c7", "#f9d1af", "#f7d814"],
 });
