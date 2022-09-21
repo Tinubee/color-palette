5726ac91030a1d4a215da426ca50c102ef6aede4
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-09-21 21:49:00 +0900

    Updates

---
+++ precache-manifest.1ac219bfb3e7eacd7c598dfee5ed3c12.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "33efce9268b3fe9194a0",
+    "url": "/color-palette/static/js/main.33efce92.chunk.js"
+  },
+  {
+    "revision": "49a3d7cd0b3041d248ea",
+    "url": "/color-palette/static/js/1.49a3d7cd.chunk.js"
+  },
+  {
+    "revision": "6d625f310382dd715922",
+    "url": "/color-palette/static/js/runtime~main.6d625f31.js"
+  },
+  {
+    "revision": "2a7863335fecb5c18a924db2badefa39",
+    "url": "/color-palette/index.html"
+  }
+];
